9b06dcb32259b207e2b920973420fb2764fbd202
John Hu <hszqqq12@gmail.com>
2020-06-13 16:13:16 +0800

    Disable singlefile comment and canonical link

--- extension/core/content/content-main.js
+++ extension/core/content/content-main.js
@@ -117,8 +117,8 @@ this.singlefile.extension.core.content.main = this.singlefile.extension.core.con
 		ui.onStartPage(options);
 		processor = new singlefile.lib.SingleFile(options);
 		const preInitializationPromises = [];
-		options.insertSingleFileComment = true;
-		options.insertCanonicalLink = true;
+		// options.insertSingleFileComment = true;
+		// options.insertCanonicalLink = true;
 		if (!options.saveRawPage) {
 			if (!options.removeFrames && frames && window.frames && window.frames.length) {
 				let frameTreePromise;
